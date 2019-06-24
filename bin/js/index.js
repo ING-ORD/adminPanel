75bce62de43ba79523f79db3e157dedc4d068860
LAPTOP-V1FCBOG0\dns2018 <ignat.markov345@gmail.com>
2019-06-24 16:57:52 +0500

    up designe #1

--- bin/js/index.js
+++ bin/js/index.js
@@ -1,7 +1,20 @@
+function CreateElementHTML(tag,text,attributes){
+    var is_atr = ["class","id","name","src","alt"];
+    var html = document.createElement(tag);
+    html.innerHTML = text;
+    for (var key in attributes){
+        if(is_atr.indexOf(key)!=-1){
+           html.setAttribute(key,attributes[key])
+        }
+    }
+    return html;
+};
+
+
 $(document).ready(function () {
-    $("div.container").html("<p>Hello, World(script JQuery)</p>");
-    
+    $(".addDateBase").click(function(){
+        $(this).slideUp(function(){
+            $(".container").append(CreateElementHTML("div","new block",{"class":"dd"}))
+        });
+    });
 });
-
-
-
