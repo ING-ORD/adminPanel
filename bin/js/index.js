17b90ac3ecd0eb4ae746eb1baf38cb29cc1688bc
MaGl <ignat.markov345@gmail.com>
2019-06-25 21:18:52 +0500

    ajax + create elements by clicked

--- bin/js/index.js
+++ bin/js/index.js
@@ -9,12 +9,52 @@ function CreateElementHTML(tag,text,attributes){
     }
     return html;
 };
+function createEll_ContentBlock (){
+    var day__tool = CreateElementHTML("div","",{ "class":"content-block-day__tool"});
+    var block__text = CreateElementHTML("div","Группа",{"class":"content-block__text"});
+    var contentBlock = CreateElementHTML("div",block__text.outerHTML,{"class":"content-block"});
+    var daysItems = CreateElementHTML("div","",{"class":"wrap-days-items"});
 
-
-$(document).ready(function () {
-    $(".addDateBase").click(function(){
-        $(this).slideUp(function(){
-            $(".container").append(CreateElementHTML("div","new block",{"class":"dd"}))
-        });
+    ["Понедельник","Вторник","Среда","Четверг","Пятница","Суббота"].forEach(function(item) {
+        var days__text = CreateElementHTML("div",item,{"class":"day-items"});
+        contentBlock.innerHTML += CreateElementHTML("div",day__tool.outerHTML + days__text.outerHTML + daysItems.outerHTML,{"class":"content-block-day"}).outerHTML;
     });
-});
+
+    return contentBlock;
+}
+
+function createEll_DaysItems(){
+    items = CreateElementHTML("div","1",{"class":"days-items"})
+    return items
+}
+
+function ajaxSeccess(data){
+    var answer = data;
+    document.querySelector(".block-header__text").innerHTML ="(PHP REQUEST:"+answer+")"
+}
+
+function ajaxRequest(){
+			$.ajax({
+				url: "bin/php/ajaxAnswer.php",
+				type:"POST",
+				data: "REQUEST1" ,
+				datatype: "html",
+				success: ajaxSeccess
+			});
+			// console.log(parametrs);
+		}
+
+/*$(document).ready(function () {*/
+    $(".block-header__adder-bd").click(function(){
+        $(".wrap-content-block").append(createEll_ContentBlock());
+        onClickDayTool()
+        ajaxRequest()
+    });
+function onClickDayTool(){
+    $(".content-block-day__tool").off("click");
+    $(".content-block-day__tool").click(function(){
+        this.parentElement.querySelector(".wrap-days-items").innerHTML += createEll_DaysItems().outerHTML;
+        ajaxRequest()
+    });
+  }
+/*});*/
